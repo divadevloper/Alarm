f5e9d437ed58c23e74fcfad675a98ff7aea5d5b8
Danicode <ariyoesther2009@gmail.com>
2024-04-01 16:42:34 +0100

    fixed something

--- function.js
+++ function.js
@@ -2,8 +2,8 @@ let select = document.getElementById('select')
 let currenttime = document.getElementById('myh1')
 let selectt = document.getElementById('selectt')
 let selecttt = document.getElementById("selecttt")
-let date = new Date
-let myh1 = document.getElementById('myh1')
+let ringtone = new Audio("Drake-Slime-You-Out-Ft-SZA-(JustNaija.com).mp3")
+let myh2 = document.getElementById('myh2')
 
 for (let index = 0; index <= 24; index++) {
     index = index < 10 ? "0" + index : index
@@ -12,7 +12,6 @@ for (let index = 0; index <= 24; index++) {
 }
 for (let index = 0; index <= 60; index++) {
     index = index < 10 ? "0" + index : index
-
     selectt.innerHTML += `<option value="${index}"> ${index}</option>`
 
 }
@@ -21,12 +20,14 @@ for (let index = 0; index <= 1; index++) {
     selecttt.innerHTML += `<option value="${ampam}"> ${ampam}</option>`;
 
 }
-setInterval(() => {
+let date = new Date
+
+let Alarm = setInterval(() => {
     let date = new Date,
         h = date.getHours(),
         m = date.getMinutes()
-       se = date.getSeconds()
-      ampam = "PM"
+    se = date.getSeconds()
+    ampam = "PM"
 
     if (h >= 24) {
         h = h = 24
@@ -36,11 +37,29 @@ setInterval(() => {
     h = h < 10 ? "0" + h : h
     m = m < 10 ? "0" + m : m
     se = se < 10 ? "0" + se : se
-    currenttime.innerHTML = (`${h}:${m} ${se} ${ampam}`);
+    currenttime.innerHTML = (`${h}:${m}:${se} ${ampam}`);
+
+    if (select.value == date.getHours() && selectt.value == date.getMinutes()) {
+        ringtone.play()
+    }
 }, 1000);
 
 
 function start() {
-    let time = `${select.value}:${selectt.value} ${selecttt.value} `
-    console.log(time);
+    if (select.value == '' && selectt.value == '') {
+        alert("You haven't set an alarm yet")
+    }
+    else if (select.value < date.getHours()) {
+        alert('Invaild Time')
+
+    }
+    else {
+        alert('An alarm has been set')
+    }
+
+
+}
+function stopalarm() {
+    clearInterval(Alarm)
+    ringtone.pause()
 }
